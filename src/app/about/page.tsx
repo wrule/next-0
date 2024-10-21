758eb2c96a0e4d3904e291ca402dad2fdd8077fc
jimao <shihao.gu@perfma.com>
2024-10-21 12:04:15 +0800

    访问计数

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,5 +2,14 @@ import fs from 'fs';
 
 export default
 function About() {
-  return <div>{fs.readFileSync('/Users/jimao/next-0/package.json', 'utf8')}</div>;
+  let state: any = { };
+  try {
+    const jsonText = fs.readFileSync('state.json', 'utf8');
+    state = JSON.parse(jsonText);
+  } catch (error) {
+    console.error(error);
+  }
+  state.count = (state.count ?? 0) + 1;
+  fs.writeFileSync('state.json', JSON.stringify(state), 'utf8');
+  return <div>{state.count}</div>;
 }
