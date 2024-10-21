4ed665aa1e1fb69e03486ae30ba8dc68eb56de5a
jimao <shihao.gu@perfma.com>
2024-10-21 11:23:27 +0800

    你好,世界

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li>你好，世界</li>
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
